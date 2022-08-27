b5ff06bfb7120f2a7279689d0ef42d6f2f0a5137
Stian-Egeli <100123497+Stian-Egeli@users.noreply.github.com>
2022-08-27 12:54:54 +0200

    Finished MA

--- js/question-2.js
+++ js/question-2.js
@@ -1,31 +1,34 @@
 //API-Key 2c71ffe2f4004e31a6503d0d12fd525b
 
-//https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=2c71ffe2f4004e31a6503d0d12fd525b
-
 const resultsContainer = document.querySelector(".display");
 
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=2c71ffe2f4004e31a6503d0d12fd525b";
 
-async function getData(){
+setTimeout (async function getData(){
     try{
         const response = await fetch(url)
+
         const result = await response.json();
+
         const detail = result.results;
 
-        resultsContainer.innerHTML = " ";
+        console.log(detail);
+
+        resultsContainer.innerHTML = "";
 
         for(let i = 0; i < 8; i++){
             if (i === 8){
                 break;
             }
-            resultsContainer.innerHTML += `<div>${detail[i].name}</div>`
+            resultsContainer.innerHTML += `<div>Name: ${detail[i].name}      Rating: ${detail[i].rating}     Number Of Tags: ${detail[i].tags.length}</div>`
         }
 
 
     } catch (error){
         console.log(error);
+        resultsContainer.innerHTML = "There was an Error!"
     }
-}
+}, 2000);
 
 getData();
 
